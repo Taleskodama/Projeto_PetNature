f18113477db5c71b7193d248752a266874b5b186
Thiago <thiagorovari@gmail.com>
2025-02-26 01:39:09 -0300

    Add EditarUsuarioComponent and integrate into app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,6 +17,7 @@ import { TelaPrincipalComponent } from './components/tela-principal/tela-princip
 import { PerfilComponent } from './components/perfil/perfil.component';
 import {HeaderComponent} from './shared/components/header/header.component';
 import { UsuariosAdminComponent } from './components/usuarios-admin/usuarios-admin.component';
+import { EditarUsuarioComponent } from './src/app/components/editar-usuario/editar-usuario.component';
 
 
 @NgModule({
@@ -30,7 +31,8 @@ import { UsuariosAdminComponent } from './components/usuarios-admin/usuarios-adm
     TelaPrincipalComponent,
     PerfilComponent,
     HeaderComponent,
-    UsuariosAdminComponent
+    UsuariosAdminComponent,
+    EditarUsuarioComponent
   
   ],
   imports: [
