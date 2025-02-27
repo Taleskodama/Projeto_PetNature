6a5522b984acab49e1b7bca7890c9e5b9ac43b63
Thiago <thiagorovari@gmail.com>
2025-02-27 15:27:53 -0300

    Barra de pesquisa não funcionando

--- src/app/components/usuarios-admin/usuarios-admin.component.ts
+++ src/app/components/usuarios-admin/usuarios-admin.component.ts
@@ -8,6 +8,9 @@ import { AuthService } from '../../shared/services/auth.service';
 })
 export class UsuariosAdminComponent {
   users: any[] = [];
+  searchTerm: string = '';
+  filteredUsers: any[] = [];
+  searchQuery: string = '';
 
   constructor(private userService: AuthService) {}
 
@@ -16,5 +19,24 @@ export class UsuariosAdminComponent {
       console.log('dados recebidos', data);
       this.users = data;
     });
+  }
+  filterUsers(): void {
+    const query = this.searchQuery.trim().toLowerCase();
+    const sanitizedQuery = query.replace(/[\.\-]/g, '');
+  
+    if (sanitizedQuery === '') {
+      // Se não houver pesquisa, exibe a página atual normalmente
+      this.userService.getUserList().subscribe(data => {
+        this.users = data;
+      });
+    } else {
+      // Filtra sobre todos os usuários
+      const filteredUsers = this.users.filter(user => {
+        const titleMatch = user.name ? user.name.toLowerCase().includes(sanitizedQuery) : false;
+        return titleMatch;
+      });
+  
+      
+    }
   }
 }
