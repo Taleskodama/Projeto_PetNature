f9ff52afca5ba9cdbe755d22974779aa61163d24
Thiago <thiagorovari@gmail.com>
2025-02-27 10:00:33 -0300

    Adicionando tipo de usuário e código. Faltando função para editar o usuário

--- src/app/components/perfil/perfil.component.ts
+++ src/app/components/perfil/perfil.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../shared/services/auth.service';
+import { UserInterface } from '../../shared/interfaces/user-interface';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-perfil',
   templateUrl: './perfil.component.html',
   styleUrl: './perfil.component.scss'
 })
-export class PerfilComponent {
+export class PerfilComponent implements OnInit {
+  user$: Observable<UserInterface | null> = of (null);
 
+  constructor(private authService: AuthService) {}
+
+  ngOnInit(): void {
+    this.user$ = this.authService.getUserData();
+  
+}
 }
